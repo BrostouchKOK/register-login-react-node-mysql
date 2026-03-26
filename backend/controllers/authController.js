e8b93ff9973f9615fe0d425de70cc5b9f5c55f7b
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-03-26 13:23:18 +0700

    completed login api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import db from "../config/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 const register = async (req, res) => {
   try {
@@ -33,6 +34,31 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
+    const {email, password} = req.body;
+    const [rows] = await db.query("SELECT * FROM users WHERE email = ?",[email]);
+    if(rows.length === 0){
+        return res.status(404).json({
+            success: false,
+            message: "User doesn't exist"
+        })
+    }
+
+    const isMatch = await bcrypt.compare(password,rows[0].password);
+    if(!isMatch){
+        return res.status(401).json({
+            success: false,
+            message:"Wrong password!"
+        })
+    }
+
+    // create token
+    const token = jwt.sign({id: rows[0].id}, process.env.JWT_KEY, {expiresIn: '3h'})
+    
+    return res.status(201).json({
+        success: true,
+        message: "Login successfully",
+        token
+    })
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Server error", error });
