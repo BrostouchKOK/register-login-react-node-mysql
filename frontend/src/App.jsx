ab565c408bb1693d3af3804866d7b0fb3bb95574
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-03-26 10:13:18 +0700

    initial frontend and backend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+const App = () => {
+  return (
+    <>
+      App
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
